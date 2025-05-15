1e8e53a31ba7db5e0d56cb31a037b01ae5edc674
Alessio Morello <alessio.morello@hotmail.com>
2025-05-15 14:46:34 +0200

    aggiunto vettore

--- esercizio.php
+++ esercizio.php
@@ -15,13 +15,14 @@
         'eta'           =>  34,
         'luogo'         => 'Palermo',
         'citta'         => 'Bologna',
-        'musica'        => 'hip-hop'
     );
+
+    $musica = ['hip-hop', 'rap', 'classico', 'lirica'];
 
     echo '<h1>' . $persona['name'] . ' ' . $persona['cognome'] . '</h1><br>' . PHP_EOL;
     echo '<p>Ciao a tutti, mi chiamo ' . $persona['name'] . ' ' . $persona['cognome'] . ', per gli amici ' . $persona['soprannome'] . '.<br>' . PHP_EOL;
     echo 'Ho ' . $persona['eta'] . ' anni, sono nato a ' . $persona['luogo'] . ' ma vivo a ' . $persona['citta'] . ' da quando avevo 6 anni.<br>' . PHP_EOL;
-    echo 'Il mio genere musicale preferito è ' . $persona['musica'] . ', ma ascolto tranquillamente qualsiasi musica (purchè sia orecchiabile).</p><br>' . PHP_EOL;
+    echo 'Il mio genere musicale preferito è ' . $musica[0] . '/' . $musica[1] . ', ma ascolto tranquillamente qualsiasi musica (purchè sia orecchiabile).</p><br>' . PHP_EOL;
 
     echo "</body>";
     echo "</html>";
