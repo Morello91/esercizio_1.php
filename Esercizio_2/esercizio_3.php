33cc9ca9532e69cce2cae4043eba0536752e9669
Alessio Morello <alessio.morello@hotmail.com>
2025-05-15 19:41:58 +0200

    esercizio 3 completato

--- Esercizio_2/esercizio_3.php
+++ Esercizio_2/esercizio_3.php
@@ -2,3 +2,31 @@
 
 
 
+
+
+<?php
+
+// $a = null;
+// $b = null;
+// $c = null;
+
+$altezza = $_GET["a"];
+$base = $_GET["b"];
+$ipotenusa = $_GET["i"];
+
+echo "<html>" . PHP_EOL;
+echo "<head>" . PHP_EOL;
+echo "<link rel='stylesheet' href='style.css'>" . PHP_EOL;
+echo "<title>Esercicio</title>" . PHP_EOL;
+echo "</head>" . PHP_EOL;
+echo "<body>" . PHP_EOL;
+echo "<h1>Area del triangolo rettangolo</h1>" . PHP_EOL;
+
+echo "<p>L'area del triangolo è: " . $altezza * $base / 2 . "</p>" . PHP_EOL; 
+
+$ipotenusa = round(sqrt($altezza**2 + $base**2));
+
+echo "<p>L'ipotenusa del rettangolo è: " . $ipotenusa . "</p>" . PHP_EOL;
+
+echo "</body>" . PHP_EOL;
+echo "</html>" . PHP_EOL;
